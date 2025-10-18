865f8810205bee7e5df729ae698ab7c2b9f85381
Benito <ntambube@gmail.com>
2025-10-18 13:22:59 +0100

    Redesign page connexion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,39 +18,80 @@ function Login({ pageTitle }) {
     document.querySelector('title').innerHTML = pageTitle
     return (
         <>
-            <div className="login-container">
-                <div className="login-left">
-                    <div className="logo"></div>
-                    <div className="login-title">
-                        <h1>Connexion Admin</h1>
-                        <p>Veuillez saisir vos identifiants pour vous connecter</p>
+            {/*  <div className="login-container">
+                <div className="login-item">
+                    <div className="login-left">
+                        <div className="logo"></div>
+                        <div className="login-title">
+                            <h1>Connexion Admin</h1>
+                            <p>Veuillez saisir vos identifiants pour vous connecter</p>
+                        </div>
+                        <div className="input-login-container">
+                            <Input
+                                placeholder={"Email"}
+                                value={email}
+                                type="email"
+                                onChange={handleEmail}
+                                className={"input input-login"}
+                            />
+                            <Input
+                                placeholder={"Mot de passe"}
+                                value={email} type="password"
+                                onChange={handlePassword}
+                                className={"input input-login"}
+                                id={"user-password"}
+                                id_i={"show-password"}
+                            />
+                            <Button
+                                placeholder={"Se Connecter"}
+                                type="submit"
+                                className={"button button-one login-button"} /
+                            >
+                        </div>
+                        <p className="forgot-password"><a href="/">Mot de passe oublié ?</a></p>
                     </div>
-                    <div className="input-login-container">
-                        <Input
-                            placeholder={"Email"}
-                            value={email}
-                            type="email"
-                            onChange={handleEmail}
-                            className={"input input-login"}
-                        />
-                        <Input
-                            placeholder={"Mot de passe"}
-                            value={email} type="password"
-                            onChange={handlePassword}
-                            className={"input input-login"}
-                            id={"user-password"}
-                            id_i={"show-password"}
-                        />
-                        <Button
-                            placeholder={"Se Connecter"}
-                            type="submit"
-                            className={"button button-one login-button"} /
-                        >
-                    </div>
-                    <p className="forgot-password"><a href="/">Mot de passe oublié ?</a></p>
-                </div>
 
-                <div className="login-right"></div>
+                    <div className="login-right"></div>
+                </div>
+            </div>
+        */}
+
+            <div className="login-container">
+                <div className="login-item">
+                    <div className="login-left">
+                        <div className="login-title">
+                            <h1>Connexion Admin</h1>
+                            <p>Veuillez saisir vos identifiants</p>
+                        </div>
+
+                        <form action="">
+                            <Input
+                                placeholder={"Email"}
+                                value={email}
+                                type="email"
+                                onChange={handleEmail}
+                                className={"input input-login"}
+                            />
+                            <Input
+                                placeholder={"Mot de passe"}
+                                value={email} type="password"
+                                onChange={handlePassword}
+                                className={"input input-login"}
+                                id={"user-password"}
+                                id_i={"show-password"}
+                            />
+                            <p className="forgot-password">
+                                <a href="#">Mot de passe oublié ?</a>
+                            </p><Button
+                                placeholder={"Se Connecter"}
+                                type="submit"
+                                className={"button button-one login-button"} /
+                            >
+                        </form>
+                    </div>
+
+                    <div className="login-right"></div>
+                </div>
             </div>
         </>
     )
